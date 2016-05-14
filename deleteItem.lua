8578b9c43471c1dcd3f32a45e628346cfa748c9b
Leonardo Caraballo <leo.cara@gmail.com>
2016-05-14 16:02:49 -0300

    Add files via upload

---
+++ deleteItem.lua
@@ -0,0 +1,20 @@
+--My First Wow Addon 2016-05-14
+--Delete a specific item from player inventory without confirmation
+--Created from pieces of codes extracted from forums about wow and lua topics
+
+SLASH_DELETEITEM1="/deleteItem"
+SlashCmdList["DELETEITEM"] = function(msg)
+	for bag=0, NUM_BAG_SLOTS do
+		for bagSlots=1, GetContainerNumSlots(bag) do
+			local itemlink = GetContainerItemLink(bag, bagSlots)
+			local unusedTexture, itemCount = GetContainerItemInfo(bag, bagSlots)
+			if (itemlink) then
+					local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, vendorPrice = GetItemInfo(itemlink)
+					if (itemLink==msg) then
+							PickupContainerItem(bag,bagSlots);
+							DeleteCursorItem();
+					end
+		  end
+		end
+	end
+end
